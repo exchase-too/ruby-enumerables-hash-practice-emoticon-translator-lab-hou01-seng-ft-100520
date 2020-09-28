1137a2da801e3a24ae2e769e64cedbd2771d5108
Eric Smith <eric.c.smith@sbcglobal.net>
2020-09-28 14:34:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,48 @@
 # require modules here
+require 'pry' 
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(emoticon_file)
+  emoticon_hash = YAML.load_file(emoticon_file)
+  final_hash = {}
+  emoticon_hash.each do |(key, value)|
+#    final_hash.push({key => {:english => value[0], :japanese => value[1]}})
+    final_hash[key] = {}
+    final_hash[key][:english] = value[0]
+    final_hash[key][:japanese] = value[1]
+  #  binding.pry
+  end
+  final_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon (emoticon_file, english_text)
+  temp_hash = load_library(emoticon_file)
+  apology_string = "Sorry, that emoticon was not found"
+  temp_hash.each do |key,value|
+    index_hash = value
+    binding.pry
+    if temp_hash.key.to_s == english_text
+      binding.pry
+      return index_hash[1].value 
+    end
+  end
+  apology_string
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(emoticon_file, japan_emot)
+  temp_hash = load_library(emoticon_file)
+  apology_string = "Sorry, that emoticon was not found"
+  temp_hash.each do |key,value|
+    temp_hash[key].each do |nest_key,nest_value|
+  #    binding.pry
+      if nest_value == japan_emot
+        return key.to_s 
+  #      binding.pry
+      end
+ #     binding.pry 
+    end
+  end
+  apology_string
 end
+
+
