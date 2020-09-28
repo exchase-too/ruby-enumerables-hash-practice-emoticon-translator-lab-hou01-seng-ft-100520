e9902746b3d3313c6556f460aa59f4d915fdb56e
Eric Smith <eric.c.smith@sbcglobal.net>
2020-09-28 14:55:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,7 +25,7 @@ def get_japanese_emoticon (emoticon_file, english_emot)
       binding.pry
       if index_hash[:english] == english_emot
       binding.pry
-      return index_hash[1] 
+      return index_hash[:japanese] 
       end
     end
   end
